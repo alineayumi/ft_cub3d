02acbbe0fa175bfc1991b1b727528f5fd58a8d78
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2020-11-09 13:15:53 -0300

    feat: hooks

---
+++ 02_hooks/hooks.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afukuhar <afukuhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 17:15:35 by afukuhar          #+#    #+#             */
+/*   Updated: 2020/11/03 17:25:03 by afukuhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef 	HOOKS_H
+# define HOOKS_H
+
+#include "../mlx/mlx.h"
+#include <stdio.h>
+
+typedef struct	s_vars
+{
+	void		*mlx;
+	void		*win;
+}				t_vars;
+
+#endif
---
+++ 02_hooks/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afukuhar <afukuhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 17:14:42 by afukuhar          #+#    #+#             */
+/*   Updated: 2020/11/03 17:25:38 by afukuhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hooks.h"
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("Hello from key hook!\n");
+}
+
+int	main(void)
+{
+	t_vars vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello World!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
+}
