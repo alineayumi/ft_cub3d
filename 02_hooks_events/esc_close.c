9d9f63d7dea37d644bc00cc511689438f5c7b964
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2020-11-18 18:59:27 -0300

    feat: close window with esc and x

--- 02_hooks_events/esc_close.c
+++ 02_hooks_events/esc_close.c
@@ -6,16 +6,25 @@
 /*   By: afukuhar <afukuhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 00:14:01 by afukuhar          #+#    #+#             */
-/*   Updated: 2020/11/18 00:17:03 by afukuhar         ###   ########.fr       */
+/*   Updated: 2020/11/18 18:58:53 by afukuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hooks.h"
 
-int             close(int keycode, t_vars *vars)
+int             close()
 {
+	printf("closing with x");
+	exit(1);
+	return (0);
+}
+
+int				esc_close(int keycode, t_vars *vars)
+{
+	printf("closing with esc");
 	if (keycode == KEY_ESC)
-    	mlx_destroy_window(vars->mlx, vars->win);
+		exit(1);
+	return (0);
 }
 
 int             main(void)
@@ -24,6 +33,7 @@ int             main(void)
 
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 500, 500, "Hello world!");
-    mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+    mlx_hook(vars.win, KeyPress, MASK_KEYPRESS, close, &vars);
+	mlx_hook(vars.win, DestroyNotify, MASK_BUTTONPRESS, close, &vars);
     mlx_loop(vars.mlx);
 } 
